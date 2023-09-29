8b0ef1c28aed2d3e91fcdf54562b7959e1776f76
RACagbay <145987922+RACagbay@users.noreply.github.com>
2023-09-29 08:06:02 +0800

    Add files via upload

---
+++ labAct#1.py
@@ -0,0 +1,83 @@
+print("LabAct1 - First Python Program")
+print("Cagbay, Ronnel A.")
+print("CITCS")
+print("1B")
+print("=================================================")
+
+lbs = 300
+print("Weigth in pounds (lbs): ", end="")
+print(lbs)
+kg = lbs * 0.454
+print("Weight converted to kilograms (kg): ", end="")
+print(kg)
+print("==================================================")
+length = 21
+print("Length in miles (mi): ", end="")
+print(length)
+km = length * 1.609344
+print("Length in kilometers (km): ", end="")
+print(km)
+print("==================================================")
+F = 9900
+print("Temperature in Fahrenheit (°F): ", end="")
+print(F)
+C = (9900 - 32) * 5/9
+print("Temperature in Celsius (°C): ", end="")
+print(C)
+print("===================================================")
+
+a = 22
+b = 19
+c = 20
+d = 20
+e = 18
+f = 21
+g = 23
+h = 25
+i = 19
+j = 21
+print("Age of the student 1 is: ", end="")
+print(a)
+print("Age of the student 2 is: ", end="")
+print(b)
+print("Age of the student 3 is: ", end="")
+print(c)
+print("Age of the student 4 is: ", end="")
+print(d)
+print("Age of the student 5 is: ", end="")
+print(e)
+print("Age of the student 6 is: ", end="")
+print(f)
+print("Age of the student 7 is: ", end="")
+print(g)
+print("Age of the student 8 is: ", end="")
+print(h)
+print("Age of the student 9 is: ", end="")
+print(i)
+print("Age of the student 10 is: ", end="")
+print(j)
+total = a + b + c + d + e + f + g + h + i + j
+ave = total / 10
+print("The average age of the Student is: ", end="")
+print(ave)
+print("===================================================")
+print("Once upon a time in a small kingdom called demacia, there was peace and quite place.")
+print("And there was a warrior and his name is Guts, he was known as the powerful warrior in the kingdom.")
+print("One afternoon, he decied to go to a journey.")
+print("As a loyal warrior of the the village of demacia, Guts will have a revenge for what happen to his village.")
+print("So, Guts goes to the forest and meet luci.")
+print("''Luci is the friend of Guts.''")
+print("and they continue thier journey to seek reverge to the demon king who killed the king of the village who is Arthur.")
+print("Along the way Guts and luci talking about how they will kill the demon king named Lucifer.")
+print("And they stumbled to a small village with a one strong warrior too and his name is Amber.")
+print("Then amber recognize Guts because he is the one who save amber when he is on the mission to kill the goblins.")
+print("So, Guts and Luci talk to amber fo a mean time and encourage amber to join them to kill the demon king.")
+print("And amber replies ''Let's go i want to help you both.''")
+print("Then, Amber joined Guts and Luci to thier journey.")
+print("While walking towards the kingdom of the demon king.")
+print("As they are walking they see the demon king wondering outside the kingdom.")
+print("So, Guts, Luci and Amber quietly sneak towards the demon king.")
+print("Then Guts attacked with his big sword <wassshing....>.")
+print("And Luci attacked to with her GLowing wand and cast a spell <gooooo glowing wand fireball......>, the unanttentive demon king was hit bt both attack then recieve a critical hit.")
+print("And the last hit will be Amber with his Spear <go spear brrrr...> then the spear ended the life of the demon king.")
+print(" And the revenge is succesful then Guts cuts the demon king head as a sign of revenge.")
